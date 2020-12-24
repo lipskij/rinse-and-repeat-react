bd9872486c36e0b38ad96974c995ba3c1949cf4d
lipskij <emil.lipskij@gmail.com>
2020-12-24 13:20:45 +0200

    f-commit

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/contacts">Contacts</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
