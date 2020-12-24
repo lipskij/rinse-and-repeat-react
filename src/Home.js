c599dbb651d731b5d47f3cc778d0f53df619672d
lipskij <emil.lipskij@gmail.com>
2020-12-24 14:42:11 +0200

    img import

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,14 @@
 import './Home.css';
+import images from './images';
 
 const Home = () => {
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <div className="images">
+        {images.map(({id, src}) => <img alt='bike' key={id} src={src}/>)}
+      </div>
     </div>
   );
 };
---
+++ src/images.js
@@ -0,0 +1,7 @@
+const images = [
+  { id: 1, src: './assets/bike1.jpg' },
+  { id: 2, src: './assets/bike2.jpg' },
+  { id: 3, src: './assets/bike3.jpg' },
+];
+
+export default images;
